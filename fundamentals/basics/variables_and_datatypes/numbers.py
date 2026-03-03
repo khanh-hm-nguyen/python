c351d8e345b320c737bf4ec14a98b72c8ecb0993
Khanh Nguyen <khanh.hm.nguyen03@gmail.com>
2026-03-03 17:32:37 +0700

    basic numbers

---
+++ fundamentals/basics/variables_and_datatypes/numbers.py
@@ -0,0 +1,24 @@
+# integers
+print(2 + 3)
+print(3 - 2)
+print(3 * 2)
+print(3 / 2)
+
+#floats
+print(0.1 + 0.1)
+
+# integers and floats
+print(4 / 2) # floats
+print(1 + 2.0) # floats
+print(1 * 2.0) # floats
+print(3.00 ** 2) # floats
+
+#underscores in numbers
+universe_age = 14_000_000_000
+print(universe_age)
+
+# multiple assignment
+x, y, z = 1, 2, 3
+
+# constant
+MAX_CONNECTIONS = 5000
