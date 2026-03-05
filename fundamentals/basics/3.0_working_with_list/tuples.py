4d675abd361e1f7937d62153ad037215b27f328d
Khanh Nguyen <khanh.hm.nguyen03@gmail.com>
2026-03-05 12:09:50 +0700

    tuples

---
+++ fundamentals/basics/3.0_working_with_list/tuples.py
@@ -0,0 +1,16 @@
+# tuples = immutable list
+dimensions = (200, 50)
+print(dimensions[0])
+print(dimensions[1])
+
+# looping through all values
+for dimension in dimensions:
+    print(dimension)
+
+# writing over a tuple
+for dimension in dimensions:
+    print(dimension)
+
+dimensions = (400, 100)
+for dimension in dimensions:
+    print(dimension)
